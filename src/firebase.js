65359193ac61cf06a5fc432cdabdbf782d7eada2
Pablo <tackpablo@gmail.com>
2022-05-13 22:09:44 -0700

    Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVwro7QoguwtKpq7-bF-48mm80yCbQRm4",
+  authDomain: "nwitter-49463.firebaseapp.com",
+  projectId: "nwitter-49463",
+  storageBucket: "nwitter-49463.appspot.com",
+  messagingSenderId: "486265416684",
+  appId: "1:486265416684:web:42cd8937c527b1b0a4aec9",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
