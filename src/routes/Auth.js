f6bdcbf54be7667035725386e9de80f06ef60755
Pablo <tackpablo@gmail.com>
2022-05-14 08:12:30 -0700

    Add login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "firebase/auth";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeHandler = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    console.log(event.target);
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChangeHandler}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChangeHandler}
+        />
+        <input type="submit" value="Login" required />
+      </form>
+      <button>Continue with Google</button>
+      <button>Continue with Github</button>
+    </div>
+  );
+};
 
 export default Auth;
