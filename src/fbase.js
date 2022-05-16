024ed0cacceef2b7cd5f4308b8fb31aeb028da5f
Pablo <tackpablo@gmail.com>
2022-05-15 21:43:18 -0700

    Add nweets to firebase database

--- src/fbase.js
+++ src/fbase.js
@@ -1,7 +1,7 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const authService = getAuth();
+
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import { dbService } from "../fbase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
 
-  const onSubmitHandler = (event) => {
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+
+    setNweet("");
   };
 
   const onChangeHandler = (event) => {
