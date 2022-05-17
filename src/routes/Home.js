94d211bd80ea8662012d6b310640090e72ba1845
Pablo <tackpablo@gmail.com>
2022-05-16 21:21:31 -0700

    Add database query for nweets and render

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const q = query(collection(dbService, "nweets"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
@@ -40,6 +59,11 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>{nweet.nweet}</div>
+        ))}
+      </div>
     </div>
   );
 };
